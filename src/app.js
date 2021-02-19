958c8c9fc511652397910f3a32db4229619000e8
Andre-Meira <andre._.meira@hotmail.com>
2021-02-19 13:42:42 -0400

    Call Routers

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+//Uses
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+//call Routers
+require("./controllers/authController")(app);;
+
+
+module.exports = app;
